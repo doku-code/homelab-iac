1cb3de380ad873a2201d8e93ff553eb01bc35c92
doku-code <doku.lab.admin@gmail.com>
2026-09-13 13:25:27 -0400

    refactor(iac): organize Terraform stacks and examples

---
+++ terraform/examples/cloud-image-vm/discovery.tf
@@ -0,0 +1,8 @@
+# data "proxmox_vm" "ubuntu_template" {
+#   id        = 8101
+#   node_name = "pve-lab"
+# }
+#
+# output "ubuntu_template_name" {
+#   value = data.proxmox_vm.ubuntu_template.name
+# }
---
+++ terraform/examples/cloud-image-vm/terraform-test.tf
@@ -0,0 +1,84 @@
+resource "proxmox_download_file" "debian13_cloud" {
+  content_type = "import"
+  datastore_id = "local"
+  node_name    = "pve-lab"
+
+  file_name = "debian-13-genericcloud-amd64.qcow2"
+
+  url = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
+
+  overwrite = false
+}
+
+
+resource "proxmox_virtual_environment_vm" "terraform_test" {
+  name        = "terraform-test"
+  description = "Disposable VM managed by Terraform"
+  tags        = ["terraform", "test"]
+
+  node_name = "pve-lab"
+
+  started = true
+  on_boot = false
+
+  stop_on_destroy = true
+
+  agent {
+    enabled = false
+  }
+
+  cpu {
+    cores = 2
+    type  = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    import_from  = proxmox_download_file.debian13_cloud.id
+    size         = 20
+    discard      = "on"
+  }
+
+  scsi_hardware = "virtio-scsi-single"
+
+  network_device {
+    bridge   = "vmbr0"
+    model    = "virtio"
+    firewall = true
+  }
+
+  initialization {
+    datastore_id = "local-lvm"
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_account {
+      username = "terraform"
+
+      keys = [
+        trimspace(file(pathexpand("~/.ssh/id_ed25519.pub")))
+      ]
+    }
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  serial_device {}
+}
+
+
+output "terraform_test_vm_id" {
+  description = "VM ID automatically assigned by Proxmox"
+  value       = proxmox_virtual_environment_vm.terraform_test.vm_id
+}
